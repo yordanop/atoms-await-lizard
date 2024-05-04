bce774273c8122ad3de712af7762b6b9a8af784d
gabriela <gabysiiu@hotmail.com>
2024-05-04 09:05:45 -0700

    added: buttons genre logic

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,31 +5,30 @@ const btnContainer = document.querySelector('#button-container');
 
 function createGenreButtons(){
     const allGenres = JSON.parse(localStorage.getItem('genresList'));
-    for(let gen_i of allGenres){
-        // poner funcion de createButton en este for para que genere todos los botones
+ for(let gen_i of allGenres){
+
         createButton(gen_i.id, gen_i.name);
-        // createButton(gen_i.id, ge_i.name);
-    }
+    
 }
+    }
+
+
+
+ 
 
 
 function createButton(buttonID, buttonName){
-    // const newButton = document.createElement('button');
     const newButton = document.createElement('button');
-    // Arreglar siguiente linea
-    newButton.setAttribute('data', `'genre-id' : ${buttonID}` )
-    newButton.setAttribute('class', "button" );
-    // ponerle nombre al boton
-    // asignarle las clases necesarias de bulma para styling
-    console.group('hola');
+    newButton.setAttribute("data-genre", buttonID)
+    newButton.setAttribute('class', "button is-primary m-3" );
+    newButton.innerHTML= buttonName;
     btnContainer.appendChild(newButton);
+
 }
 
 
-// document.addEventListener("DOMContentLoaded", function(event) {
     createGenreButtons();
     
-//   });
 
 
 
